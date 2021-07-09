f74ca0231df30dc83a0faf35e2f6d312ab9ad75d
kagemeka <kagemeka1@gmail.com>
2021-07-09 20:42:09 +0900

    enhance: implement scrape free comic ranking (#31)

--- scrape/src/__main__.py
+++ scrape/src/__main__.py
@@ -14,71 +14,85 @@ import (
 from lib.adam import (
   MakeAdamDFs,
 )
+from pprint import (
+  pprint,
+)
 
-
-
-
-def create_driver(
-) -> WebDriver:
-  opt = ChromeOptions()
-  opts = [
-    '--no-sandbox',
-    '--single-process',
-    '--disable-dev-shm-usage',
-    '--homedir=/tmp',
-  ]
-  opt.headless = True
-  for o in opts:
-    opt.add_argument(o)
-  opt.binary_location = (
-    '/opt/headless-chromium'
-  )
-  driver = Chrome(
-    '/opt/chromedriver',
-    options=opt,
-  )
-  return driver
-
-
-def lambda_handler(
-  event, 
-  context,
-):
-  driver = create_driver()
-  MakeAdamDFs(driver)()
-  driver.close()
-  return {
-    "statusCode": 200,
-    "body": 'success',
-  }
 
 
 # def create_driver(
 # ) -> WebDriver:
-#   options = FirefoxOptions()
-#   options.headless = True
-#   # options.add_argument('--no-sandbox')
-
-
-#   driver = webdriver.Firefox(
-#     options=options,
+#   opt = ChromeOptions()
+#   opts = [
+#     '--no-sandbox',
+#     '--single-process',
+#     '--disable-dev-shm-usage',
+#     '--homedir=/tmp',
+#   ]
+#   opt.headless = True
+#   for o in opts:
+#     opt.add_argument(o)
+#   opt.binary_location = (
+#     '/opt/headless-chromium'
+#   )
+#   driver = Chrome(
+#     '/opt/chromedriver',
+#     options=opt,
 #   )
 #   return driver
 
 
-  
+# def lambda_handler(
+#   event, 
+#   context,
+# ):
+#   driver = create_driver()
+#   MakeAdamDFs(driver)()
+#   driver.close()
+#   return {
+#     "statusCode": 200,
+#     "body": 'success',
+#   }
+
+
+def create_driver(
+) -> WebDriver:
+  options = FirefoxOptions()
+  options.headless = True
+  driver = Firefox(
+    options=options,
+  )
+  return driver
+
+
+
+
+import typing
+import dataclasses
+
+from lib.pixiv.scrape.free_ranking import (
+  ScrapeFreeRanking,
+)
+
+
 def main():
   site_url ="https://comic.pixiv.net/"
 
   import time
   s = time.time()
   driver = create_driver()
+  scrape = ScrapeFreeRanking(
+    driver,
+  )
+  comics = scrape()
+  pprint(comics)
 
-  MakeAdamDFs(driver)()
+
+  # MakeAdamDFs(driver)()
   driver.close()
   print(time.time() - s)
 
  
 
-# if __name__ == '__main__':
-#   main()
+if __name__ == '__main__':
+  main()
