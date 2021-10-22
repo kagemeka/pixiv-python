9494d9e6371157576522af55bf8e55b77d99fd1d
kagemeka <kagemeka1@gmail.com>
2021-10-22 10:51:36 +0900

    bugfix: handle the case a comic has been deleted from pixiv

--- scrape/src/lib/pixiv/scrape/__init__.py
+++ scrape/src/lib/pixiv/scrape/__init__.py
@@ -1,3 +1,6 @@
-from .free_comic import FreeComic, scrape_free_comic
+from .free_comic import (
+    FreeComic, 
+    scrape_free_comic,
+    scrape_free_comics,
+)
 from .free_ranking import scrape_free_ranking
-from .free_comics import scrape_free_comics
